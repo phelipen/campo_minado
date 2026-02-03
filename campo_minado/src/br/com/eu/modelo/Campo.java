6f7b7cecfdf847d2a04553383a15a40808bc8816
Phelipe <Phelipe@Phelipe>
2026-02-02 22:36:38 -0300

    first commit

---
+++ campo_minado/src/br/com/eu/modelo/Campo.java
@@ -0,0 +1,43 @@
+package br.com.eu.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campo {
+	
+	private final int linha;
+	private final int coluna;
+	
+	private boolean minado = false;
+	private boolean aberto = false;
+	private boolean marcado = false;
+	
+	private List<Campo> vizinhos = new ArrayList<Campo>();
+
+	Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	boolean adicionarVizinho(Campo possivelVizinho) {
+		boolean linhaDiferente = linha != possivelVizinho.linha;
+		boolean colunaDiferente = coluna != possivelVizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+		
+		int deltaLinha = Math.abs(linha - possivelVizinho.linha);
+		int deltaColuna = Math.abs(coluna - possivelVizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha;
+		
+		if(deltaGeral == 1 && !diagonal) {
+			vizinhos.add(possivelVizinho);
+			return true;
+		} else if (deltaGeral == 2 && diagonal){
+			vizinhos.add(possivelVizinho);
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+
+}
